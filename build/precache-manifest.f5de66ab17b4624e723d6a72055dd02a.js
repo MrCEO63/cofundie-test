ae8116961515926db238202d08da969678383c7c
AllStackDev <nedu63ima@gmail.com>
2020-04-06 12:38:50 +0000

    built

---
+++ build/precache-manifest.f5de66ab17b4624e723d6a72055dd02a.js
@@ -0,0 +1,158 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52e45fae9c1c65602cdaff31b24e7a9e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8add5248ab1a1f484892",
+    "url": "/static/js/2.417401da.chunk.js"
+  },
+  {
+    "revision": "49f6891b97f1bee46ae8c9390d4b4743",
+    "url": "/static/js/2.417401da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e57394cd64d1fc993f0",
+    "url": "/static/js/main.b9133c79.chunk.js"
+  },
+  {
+    "revision": "ef536649d55b71fe5755",
+    "url": "/static/js/runtime-main.52022b70.js"
+  },
+  {
+    "revision": "7d8e17a353e621e426f047a6d587d646",
+    "url": "/static/media/1.7d8e17a3.jpg"
+  },
+  {
+    "revision": "ace1a74da4df9e118cc3bc38f8c69647",
+    "url": "/static/media/1.ace1a74d.jpg"
+  },
+  {
+    "revision": "9d9a4f61aa4c4a54419e973e10caad6a",
+    "url": "/static/media/2.9d9a4f61.jpg"
+  },
+  {
+    "revision": "d06816cb0c8262555190120c1e33524b",
+    "url": "/static/media/2.d06816cb.jpg"
+  },
+  {
+    "revision": "fe682da807bd2feddeb3c48c3d58eaf0",
+    "url": "/static/media/2.fe682da8.jpg"
+  },
+  {
+    "revision": "1827ae3b096218af02544eb53890a9cb",
+    "url": "/static/media/3.1827ae3b.jpg"
+  },
+  {
+    "revision": "aeed19901b0406186262a41c62a74e09",
+    "url": "/static/media/3.aeed1990.jpg"
+  },
+  {
+    "revision": "d7fa1a52cad3adb4964b98ffe5087d2b",
+    "url": "/static/media/3.d7fa1a52.jpg"
+  },
+  {
+    "revision": "4c0f97bbc556273fd4ac0d05e560156a",
+    "url": "/static/media/4.4c0f97bb.jpg"
+  },
+  {
+    "revision": "8223559426f9be1d740611f5c19d9925",
+    "url": "/static/media/MuseoSans300.82235594.woff2"
+  },
+  {
+    "revision": "5d0681adb7de26501f9c9a630c39f2d3",
+    "url": "/static/media/MuseoSans500.5d0681ad.woff2"
+  },
+  {
+    "revision": "7f07f183c89ee1d96f636a4993e7bee1",
+    "url": "/static/media/MuseoSans700.7f07f183.woff2"
+  },
+  {
+    "revision": "708bb2bcaf9bece91d4f6c7777165d50",
+    "url": "/static/media/amara.708bb2bc.png"
+  },
+  {
+    "revision": "d86face55a35a0e631e5cd32e58fa579",
+    "url": "/static/media/bullet.d86face5.svg"
+  },
+  {
+    "revision": "72fbf1c3f5dfdee913daa490c2b0e174",
+    "url": "/static/media/cancel.72fbf1c3.svg"
+  },
+  {
+    "revision": "d5099747f5e8b5327a37856dc2052bb0",
+    "url": "/static/media/choose.d5099747.png"
+  },
+  {
+    "revision": "19be9891b54f6a33f422779cec6bb67e",
+    "url": "/static/media/cofundie-logo.19be9891.png"
+  },
+  {
+    "revision": "b99d51473415d9565db10ee546033c13",
+    "url": "/static/media/create.b99d5147.png"
+  },
+  {
+    "revision": "1570e26344dd6ad9acd9ab06a68fff81",
+    "url": "/static/media/earn.1570e263.png"
+  },
+  {
+    "revision": "1e4f54df9f873c84e6f02d2192666ce2",
+    "url": "/static/media/facebook.1e4f54df.svg"
+  },
+  {
+    "revision": "672b59ec313e832b88c1f39102d2d6ab",
+    "url": "/static/media/hamburger.672b59ec.svg"
+  },
+  {
+    "revision": "7312aa1e6877e98c5735b173d0838037",
+    "url": "/static/media/hero-image.7312aa1e.png"
+  },
+  {
+    "revision": "e01204d079e279c69244d528f4bf0db2",
+    "url": "/static/media/instagram.e01204d0.svg"
+  },
+  {
+    "revision": "869d2df04fa35d103f563311faf1b9b6",
+    "url": "/static/media/jide.869d2df0.png"
+  },
+  {
+    "revision": "b9afc9bc863169efd7ebe1b9dad48cb0",
+    "url": "/static/media/kelvin.b9afc9bc.png"
+  },
+  {
+    "revision": "9a65cf5655383fc9752925a3b99e90c5",
+    "url": "/static/media/macbook-mobile.9a65cf56.png"
+  },
+  {
+    "revision": "ad4d8ecf4024cc2d688a9975fbd98744",
+    "url": "/static/media/macbook-tablet.ad4d8ecf.png"
+  },
+  {
+    "revision": "e60de4fcc5f76940c6a79bfdd8f82541",
+    "url": "/static/media/macbook.e60de4fc.png"
+  },
+  {
+    "revision": "f6028a677cb8b60232fa279bcd94f906",
+    "url": "/static/media/news-logos.f6028a67.png"
+  },
+  {
+    "revision": "2f3bcb872c594e619c0e26ff42d4c241",
+    "url": "/static/media/partners.2f3bcb87.png"
+  },
+  {
+    "revision": "86501282b4f1a2cf06b2527e037343db",
+    "url": "/static/media/twitter.86501282.svg"
+  },
+  {
+    "revision": "1dad855b23e69ec1dbdabfbcb4c2b62a",
+    "url": "/static/media/white-house-mobile.1dad855b.png"
+  },
+  {
+    "revision": "9f21ec5b0410decbca725abbafb302ed",
+    "url": "/static/media/white-house-tablet.9f21ec5b.png"
+  },
+  {
+    "revision": "82203cb419888a505f811d2469ae563a",
+    "url": "/static/media/white-house.82203cb4.png"
+  }
+]);
